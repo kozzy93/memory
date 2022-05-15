3b86c6ae24ba3309230fd9e397cca132affd9b89
kozga <krzysztof_ozga@poczta.fm>
2022-05-15 16:55:00 +0200

    Komponent gry

--- src/app/features/game/game.component.ts
+++ src/app/features/game/game.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Card } from "../../models/card-model";
+import { CardsService } from "../../services/cards.service";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-game',
-  templateUrl: './game.component.html',
-  styleUrls: ['./game.component.css']
+  selector: "app-game",
+  templateUrl: "./game.component.html",
+  styleUrls: ["./game.component.css"],
 })
 export class GameComponent implements OnInit {
+  cards: Card[] = [{ word: "", translation: "" }];
+  categoryId: number = 0;
+  cardIndex: number = 0;
+  showTranslation = false;
 
-  constructor() { }
+  constructor(
+    private cardsService: CardsService,
+    private activeRoute: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.activeRoute.paramMap.subscribe(param => {
+      const idString = param.get("id");
+      if (idString) {
+        const id = Number.parseInt(idString);
+        this.categoryId = id;
+        this.cardsService
+          .getCardsForCategory(id)
+          .subscribe(cards => (this.cards = cards));
+      }
+    });
   }
 
+  actualCard() {
+    return this.cards[this.cardIndex];
+  }
+
+  reverse() {
+    this.showTranslation = true;
+  }
+
+  next() {
+    if (this.cardIndex < this.cards.length) {
+      ++this.cardIndex;
+      this.showTranslation = false;
+    }
+  }
 }
